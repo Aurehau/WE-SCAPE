841e7664db89ee8626c3fbb56e4f4358232bae46
Aurehau <114416513+Aurehau@users.noreply.github.com>
2024-04-16 11:53:46 +0200

    a

--- script/script.js
+++ script/script.js
@@ -34,3 +34,13 @@ function menuDetails() {
         }
     }
 }
+
+
+/*********************visibilité option de connexion*************************/
+document.querySelector(".icone_profil").addEventListener("click", option_connexion);
+
+
+function option_connexion(event){
+	document.querySelector(".icone_profil").classList.toggle("vague");
+	document.querySelector(".option_connexion").classList.toggle("visible");
+}
--- script/test.js
+++ script/test.js
@@ -1,7 +1,7 @@
-document.querySelector(".icone_profil").addEventListener("click", option_connexion);
+/* document.querySelector(".icone_profil").addEventListener("click", option_connexion);
 
 
 function option_connexion(event){
 	document.querySelector(".icone_profil").classList.toggle("vague");
 	document.querySelector(".option_connexion").classList.toggle("visible");
-}
+} */
