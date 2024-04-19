50aab81bdafcd38389ddcada7e0e95319df8db1b
chaymabann <chayma.bann@outlook.fr>
2024-04-19 09:27:28 +0200

    update error

--- vue/vueAdminAjoutCarte.php
+++ vue/vueAdminAjoutCarte.php
@@ -27,7 +27,7 @@
                     /*réfléchir pour ajouter la miniature */
                     echo $formulaire->inputTextI('prix', 'adminAjout-prix');
                     /*réfléchir pour ajouter les valeur possible du bon */
-                    echo $formulaire->textAreaI('description', 'adminAjout-description')
+                    echo $formulaire->textAreaI('description', 'adminAjout-description');
                     echo $formulaire->inputTextI('raisons', 'adminAjout-raisons');
                     echo $formulaire->inputTextI('delai', 'adminAjout-delai');
                     echo $formulaire->inputTextI('taille', 'adminAjout-taille');
