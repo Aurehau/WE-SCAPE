8b0fc6c560faec49c3cd77448d31188b6ea6db8d
chaymabann <chayma.bann@outlook.fr>
2024-04-29 10:01:34 +0200

    update cgv

--- vue/vueCGV.php
+++ vue/vueCGV.php
@@ -10,28 +10,64 @@
 
 <div class="resultat conteneur">
         <div>
-            <h2> Objet du contrat </h2>
-            Les présentes conditions générales de vente régissent les relations contractuelles entre [Nom de votre entreprise], ci-après dénommé "le Prestataire", et tout utilisateur du site internet [Adresse de votre site], ci-après dénommé "le Client".
-    
-            <h2> Description des services </h2>
-            Le Prestataire propose des services d'escape game en extérieur, consistant en [description détaillée des services proposés].
-    
-            <h2> Tarifs et paiement </h2>
-            Les tarifs des services sont indiqués sur le site internet et sont susceptibles d'être modifiés par le Prestataire.
-            Le paiement s'effectue selon les modalités précisées lors de la réservation.
-    
-            <h2> Annulation et remboursement </h2>
-            Les conditions d'annulation et de remboursement sont précisées lors de la réservation et peuvent varier en fonction des circonstances.
-    
-            <h2> Responsabilité </h2>
-            Le Prestataire ne peut être tenu responsable des accidents, dommages ou pertes survenus lors de la pratique des activités proposées, sauf en cas de faute prouvée de sa part.
-    
-            <h2> Propriété intellectuelle </h2>
-            Tous les contenus présents sur le site internet (textes, images, vidéos, etc.) sont la propriété exclusive du Prestataire ou de ses partenaires et sont protégés par les lois relatives à la propriété intellectuelle.
-    
-            <h2> Litiges </h2>
-            Tout litige relatif aux présentes CGV sera soumis à la compétence exclusive des tribunaux compétents du lieu du siège social du Prestataire.
-        
+
+        <h2> En effectuant une réservation d'escape game sur notre site web, le client accepte les
+        conditions suivantes : </h2><br><br>
+                - Les réservations sont sujettes à disponibilité et sont soumises à des restrictions de
+                date et d'heure.
+                - Le paiement intégral est requis au moment de la réservation. Les paiements sont
+                non remboursables, sauf en cas d'annulation de la part de la société We escape.
+                - Les participants doivent arriver à l'heure convenue pour leur réservation. Tout retard
+                abusif peut entraîner l'annulation de la réservation sans remboursement.
+                - Les participants doivent respecter les règles et consignes de sécurité fournies par la
+                société We escape avant et pendant l'escape game.
+                - La société We escape se réserve le droit de refuser l'entrée à toute personne qui ne
+                respecte pas les règles établies ou qui perturbe le bon déroulement de l'escape
+                game.
+                - En cas de force majeure ou de circonstances imprévues, la société We escape se
+                réserve le droit de modifier ou d'annuler une réservation. Dans ce cas, les clients
+                seront informés dès que possible et un remboursement ou un report de la
+                réservation sera proposé.
+
+            <h2> Conditions de vente pour les cartes cadeaux :</h2>
+                En achetant une carte cadeau sur notre site web, le client accepte les conditions suivantes :<br><br>
+                <li>- La carte cadeau est valable pour une utilisation unique et ne peut être échangée
+                contre de l'argent ou un autre produit.</li>
+                <li>- La carte cadeau peut être utilisée pour l'achat de tout escape game proposé par la
+                société We escape en fonction de la somme permise par celle-ci.</li>
+                <li>- La carte cadeau est valide pendant une période déterminée, généralement un an à
+                compter de la date d'achat. Passé ce délai, la carte cadeau expire et ne peut plus
+                être utilisée.</li>
+                <li>- La société We escape décline toute responsabilité en cas de perte, de vol ou de
+                dommage de la carte cadeau.</li>
+                <li>- En cas d'utilisation frauduleuse de la carte cadeau, la société We escape se réserve
+                le droit de refuser l'utilisation de la carte et d'engager des poursuites judiciaires si
+                nécessaire.</li>
+           
+            <h2> Conditions générales d'utilisation du site web : </h2>
+                L'utilisation du site web de la société We Escape est soumise aux conditions suivantes :
+
+                <h3> Acceptation des conditions : </h3>
+                En accédant et en utilisant ce site web, vous acceptez d'être lié par ces conditions générales d'utilisation. Si vous n'acceptez pas ces conditions, veuillez ne pas utiliser ce site.    
+            
+                <h3> Utilisation autorisée : </h3>
+                Vous acceptez d'utiliser ce site web uniquement à des fins légales et conformes à ces conditions générales, ainsi qu'à toutes les lois et réglementations applicables.    
+            
+                <h3> Propriété intellectuelle : </h3>
+                Tous les contenus de ce site web, y compris mais sans s'y limiter les textes, graphiques, logos, images, clips audio et vidéo, sont la propriété exclusive de la société We Escape ou de ses concédants de licence, et sont protégés par les lois sur le droit d'auteur et les autres lois sur la propriété intellectuelle.    
+            
+                <h3> Utilisation prohibée : </h3>
+                Il est interdit d'utiliser ce site web d'une manière qui pourrait causer des dommages, des interruptions ou des dysfonctionnements, ou d'entraver l'utilisation de ce site par d'autres utilisateurs.
+
+                <h3> Limitation de responsabilité : </h3>
+                La société We Escape décline toute responsabilité quant à l'utilisation ou à l'incapacité d'utiliser ce site web, ainsi qu'à tout dommage direct, indirect, accessoire, consécutif ou punitif découlant de votre accès ou de votre utilisation de ce site.
+
+                <h3> Modifications : </h3>
+                La société We Escape se réserve le droit de modifier ou de mettre à jour ces conditions générales d'utilisation à tout moment et sans préavis. Il est de votre responsabilité de consulter régulièrement cette page pour prendre connaissance des modifications apportées.
+
+                <h3> Droit applicable : </h3>
+                Ces conditions générales d'utilisation sont régies par les lois en vigueur dans le code pénal français, et tout litige découlant de ces conditions générales sera soumis à la compétence exclusive des tribunaux de Strasbourh. En utilisant ce site web, vous consentez à ces conditions générales d'utilisation. Si vous avez des questions ou des préoccupations concernant ces conditions, veuillez nous contacter à l’adresse mail booking@we-escape.de.
+
             <br><br>
         </div>
 </div>
