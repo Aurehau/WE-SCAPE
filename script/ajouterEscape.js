e17eb00d3d91b12527422e33a08ba5231573f744
chaymabann <chayma.bann@outlook.fr>
2024-04-29 10:49:58 +0200

    update formulaires en/fr

--- script/ajouterEscape.js
+++ script/ajouterEscape.js
@@ -5,17 +5,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Écoute les clics sur l'élément remplirFrancais
     document.querySelector('.remplirFrancais').addEventListener('click', function() {
-        // Affiche le formulaire en français
-        document.getElementById('formulaireFr').style.display = 'block';
-        // Masque le formulaire en anglais
-        document.getElementById('formulaireEn').style.display = 'none';
+        // Bascule l'état du formulaire en français
+        document.getElementById('formulaireFr').style.display =
+            (document.getElementById('formulaireFr').style.display === 'block') ? 'none' : 'block';
+
+        // Si le formulaire en anglais est visible, le cache
+        if (document.getElementById('formulaireEn').style.display === 'block') {
+            document.getElementById('formulaireEn').style.display = 'none';
+        }
     });
 
     // Écoute les clics sur l'élément remplirAnglais
     document.querySelector('.remplirAnglais').addEventListener('click', function() {
-        // Affiche le formulaire en anglais
-        document.getElementById('formulaireEn').style.display = 'block';
-        // Masque le formulaire en français
-        document.getElementById('formulaireFr').style.display = 'none';
+        // Bascule l'état du formulaire en anglais
+        document.getElementById('formulaireEn').style.display =
+            (document.getElementById('formulaireEn').style.display === 'block') ? 'none' : 'block';
+
+        // Si le formulaire en français est visible, le cache
+        if (document.getElementById('formulaireFr').style.display === 'block') {
+            document.getElementById('formulaireFr').style.display = 'none';
+        }
     });
 });
