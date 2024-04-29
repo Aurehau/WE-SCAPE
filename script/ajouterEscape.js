0c6c604ba8cc8d107db6938c40eece8e0f55eec1
chaymabann <chayma.bann@outlook.fr>
2024-04-29 10:56:17 +0200

    update form en/fr affichage

--- script/ajouterEscape.js
+++ script/ajouterEscape.js
@@ -3,25 +3,28 @@
 /*************** Gestion bouton fr/en ***************/
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Écoute les clics sur l'élément remplirFrancais
+    // Selection formulaire en français
     document.querySelector('.remplirFrancais').addEventListener('click', function() {
-        // Bascule l'état du formulaire en français
+
+        // Affiche le formulaire en francais
         document.getElementById('formulaireFr').style.display =
             (document.getElementById('formulaireFr').style.display === 'block') ? 'none' : 'block';
 
-        // Si le formulaire en anglais est visible, ne pas le masquer
+        // Pouvoir afficher les deux formulaires en mm temps si les deux boutons ont été séléctionnés
         if (document.getElementById('formulaireEn').style.display === 'block') {
             return;
         }
+
     });
 
-    // Écoute les clics sur l'élément remplirAnglais
+    // Selection formulaire en anglais
     document.querySelector('.remplirAnglais').addEventListener('click', function() {
-        // Bascule l'état du formulaire en anglais
+
+        // Affiche le formulaire en anglais
         document.getElementById('formulaireEn').style.display =
             (document.getElementById('formulaireEn').style.display === 'block') ? 'none' : 'block';
 
-        // Si le formulaire en français est visible, ne pas le masquer
+        // Pouvoir afficher les deux formulaires en mm temps si les deux boutons ont été séléctionnés
         if (document.getElementById('formulaireFr').style.display === 'block') {
             return;
         }
