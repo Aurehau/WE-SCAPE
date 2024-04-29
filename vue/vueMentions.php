cb6448f4e619eb28934e6fb80a8defb3f8b60d48
chaymabann <chayma.bann@outlook.fr>
2024-04-29 09:54:19 +0200

    update mentions légales

--- vue/vueMentions.php
+++ vue/vueMentions.php
@@ -10,13 +10,23 @@
 
 <div class="resultat conteneur">
         <div>
-            <h2> Éditeur du site : </h2>[Nom de votre entreprise]
-            <h2> Siège social : </h2> [Adresse du siège social]
-            <h2> Téléphone : </h2> [Numéro de téléphone]
-            <h2> Email : </h2> [Adresse email]
-            <h2> Directeur de la publication : </h2> [Nom du directeur de la publication]
-            <h2> Hébergeur du site : </h2> [Nom de l'hébergeur] - [Adresse de l'hébergeur]
-
+            <h2> Éditeur du site : </h2>IUT Mulhouse
+            <h2> Adresse éditeur : </h2>61 Rue Albert Camus, 68200 Mulhouse
+            <h2> Téléphone de l'éditeur: </h2>03 89 33 74 00
+            <h2> Email de l'éditeur : </h2>iutmulhouse@uha.fr
+            <br><br>
+            <h2> Responsable de la publication : </h2>We-Escape GmbH
+            <h2> Directeur de la publication : </h2>Thilo Constance
+            <h2> Adresse : </h2>Am Krebsbach 2G 79241 Ihringen
+            <h2> Téléphone : </h2>07668 996660
+            <h2> E-mail : </h2>booking@we-escape.de
+            <h2> Identification TVA : </h2>DE348059564
+            <h2> Capital social : </h2>1000€
+            <br><br>
+            <h2> Hébergeur du site : </h2>IONOS
+            <h2> Adresse : </h2>7 PLACE DE LA GARE 57200 SARREGUEMINES.
+            <h2> Téléphone : </h2>09 70 80 89 11
+            <h2> E-mail : </h2>tco-order@ionos.de
             <br><br>
         </div>
 </div>
