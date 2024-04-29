8fa1df2ffd09236a0213cf87d08b2886a0bc1c0a
chaymabann <chayma.bann@outlook.fr>
2024-04-29 11:33:06 +0200

    update

--- script/ajouterEscape.js
+++ script/ajouterEscape.js
@@ -2,30 +2,59 @@
 
 /*************** Gestion bouton fr/en ***************/
 
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Selection formulaire en français
+//     document.querySelector('.remplirFrancais').addEventListener('click', function() {
+
+//         // Affiche le formulaire en francais
+//         document.getElementById('formulaireFr').style.display =
+//             (document.getElementById('formulaireFr').style.display === 'block') ? 'none' : 'block';
+
+//         // Pouvoir afficher les deux formulaires en mm temps si les deux boutons ont été séléctionnés
+//         if (document.getElementById('formulaireEn').style.display === 'block') {
+//             return;
+//         }
+
+//     });
+
+//     // Selection formulaire en anglais
+//     document.querySelector('.remplirAnglais').addEventListener('click', function() {
+
+//         // Affiche le formulaire en anglais
+//         document.getElementById('formulaireEn').style.display =
+//             (document.getElementById('formulaireEn').style.display === 'block') ? 'none' : 'block';
+
+//         // Pouvoir afficher les deux formulaires en mm temps si les deux boutons ont été séléctionnés
+//         if (document.getElementById('formulaireFr').style.display === 'block') {
+//             return;
+//         }
+//     });
+// });
+
 document.addEventListener('DOMContentLoaded', function() {
     // Selection formulaire en français
-    document.querySelector('.remplirFrancais').addEventListener('click', function() {
+    document.querySelectorAll('.remplirFrancais').addEventListener('click', function() {
 
         // Affiche le formulaire en francais
-        document.getElementById('formulaireFr').style.display =
-            (document.getElementById('formulaireFr').style.display === 'block') ? 'none' : 'block';
+        document.querySelectorAll('[id^="formulaireFr"]').style.display =
+            (document.querySelectorAll('[id^="formulaireFr"]').style.display === 'block') ? 'none' : 'block';
 
         // Pouvoir afficher les deux formulaires en mm temps si les deux boutons ont été séléctionnés
-        if (document.getElementById('formulaireEn').style.display === 'block') {
+        if (document.querySelectorAll('[id^="formulaireEn"]').style.display === 'block') {
             return;
         }
 
     });
 
     // Selection formulaire en anglais
-    document.querySelector('.remplirAnglais').addEventListener('click', function() {
+    document.querySelectorAll('.remplirAnglais').addEventListener('click', function() {
 
         // Affiche le formulaire en anglais
-        document.getElementById('formulaireEn').style.display =
-            (document.getElementById('formulaireEn').style.display === 'block') ? 'none' : 'block';
+        document.querySelectorAll('[id^="formulaireEn"]').style.display =
+            (document.querySelectorAll('[id^="formulaireEn"]').style.display === 'block') ? 'none' : 'block';
 
         // Pouvoir afficher les deux formulaires en mm temps si les deux boutons ont été séléctionnés
-        if (document.getElementById('formulaireFr').style.display === 'block') {
+        if (document.querySelectorAll('[id^="formulaireFr"]').style.display === 'block') {
             return;
         }
     });
