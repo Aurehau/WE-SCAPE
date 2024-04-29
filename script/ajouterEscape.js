14a6b00e93bb00852115be184e41df5cc76e9443
chaymabann <chayma.bann@outlook.fr>
2024-04-29 11:37:39 +0200

    update

--- script/ajouterEscape.js
+++ script/ajouterEscape.js
@@ -2,35 +2,6 @@
 
 /*************** Gestion bouton fr/en ***************/
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     // Selection formulaire en français
-//     document.querySelector('.remplirFrancais').addEventListener('click', function() {
-
-//         // Affiche le formulaire en francais
-//         document.getElementById('formulaireFr').style.display =
-//             (document.getElementById('formulaireFr').style.display === 'block') ? 'none' : 'block';
-
-//         // Pouvoir afficher les deux formulaires en mm temps si les deux boutons ont été séléctionnés
-//         if (document.getElementById('formulaireEn').style.display === 'block') {
-//             return;
-//         }
-
-//     });
-
-//     // Selection formulaire en anglais
-//     document.querySelector('.remplirAnglais').addEventListener('click', function() {
-
-//         // Affiche le formulaire en anglais
-//         document.getElementById('formulaireEn').style.display =
-//             (document.getElementById('formulaireEn').style.display === 'block') ? 'none' : 'block';
-
-//         // Pouvoir afficher les deux formulaires en mm temps si les deux boutons ont été séléctionnés
-//         if (document.getElementById('formulaireFr').style.display === 'block') {
-//             return;
-//         }
-//     });
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
     // Selection formulaire en français
     document.querySelectorAll('.remplirFrancais').forEach(function(element) {
