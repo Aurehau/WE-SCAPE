232c9f0c4533c2857f42bdc6f258e2f462c5f695
chaymabann <chayma.bann@outlook.fr>
2024-04-29 10:10:30 +0200

    update confidentialité

--- vue/vueConfidentialite.php
+++ vue/vueConfidentialite.php
@@ -11,18 +11,18 @@
 <div class="resultat conteneur">
     <div>
 
-        La société We escape attache une grande importance à la protection de la vie privée de ses utilisateurs. La présente politique de confidentialité décrit comment nous collectons, utilisons, stockons et protégeons les informations personnelles que vous nous fournissez lorsque vous utilisez notre site web.
+        La société We escape attache une grande importance à la protection de la vie privée de ses utilisateurs.<br>La présente politique de confidentialité décrit comment nous collectons, utilisons, stockons et protégeons les informations personnelles que vous nous fournissez lorsque vous utilisez notre site web.
 
         <h2> Collecte et utilisation des informations personnelles : </h2>
 
-          Nous pouvons collecter les informations personnelles suivantes lorsque vous utilisez notre site web :
+          Nous pouvons collecter les informations personnelles suivantes lorsque vous utilisez notre site web :<br><br>
           <li>Les informations que vous fournissez volontairement, telles que votre nom, votre
           adresse e-mail, votre numéro de téléphone et d'autres informations de contact
           lorsque vous créez un compte client, effectuez une réservation d'escape game ou
           achetez une carte cadeau.</li>
           <li>Les informations que nous collectons automatiquement lorsque vous naviguez sur
           notre site web, les liens sur lesquels vous cliquez.</li>
-          <li>Nous utilisons les informations personnelles collectées pour les finalités suivantes :
+          <br><br>Nous utilisons les informations personnelles collectées pour les finalités suivantes :<br><br>
           <li>Pour traiter vos réservations d'escape game et vos achats de cartes cadeaux.</li>
           <li>Pour répondre à vos demandes de renseignements et fournir un service
           clientèle.</li>
@@ -30,26 +30,26 @@
           promotionnelles, si vous avez consenti à les recevoir.</li>
           <li>Pour améliorer notre site web et personnaliser votre expérience utilisateur.</li>
           <li>Pour conserver les escape game ou autre produit sélectionné dans un panier.</li>
-          <li>Nous ne vendrons, ne louerons ni ne partagerons vos informations
+          <br><br>Nous ne vendrons, ne louerons ni ne partagerons vos informations
           personnelles avec des tiers sans votre consentement préalable, sauf dans les
-          cas suivants :</li>
-          <li><li>Pour se conformer à la loi, aux réglementations ou à une ordonnance
-          judiciaire.<li></li>
+          cas suivants :<br><br>
+          <li>Pour se conformer à la loi, aux réglementations ou à une ordonnance
+          judiciaire.</li>
           <li>Pour protéger les droits, la propriété ou la sécurité de la société We
           escape ou de ses utilisateurs.</li>
           <li>En cas de fusion, acquisition ou vente d'actifs, où vos informations
           personnelles peuvent être transférées en tant qu'actif de l'entreprise.</li>
 
+        <h2> Conservation des données </h2>
+        Nous conservons vos informations personnelles aussi longtemps que nécessaire pour remplir les finalités pour lesquelles elles ont été collectées, ainsi que pour respecter nos obligations légales et réglementaires. Si vous souhaitez que nous supprimions vos informations personnelles de nos bases de données, veuillez nous contacter à booking@we-escape.de.
+        
         <h2> Sécurité des données </h2>
-        Le Prestataire s'engage à prendre toutes les mesures nécessaires pour assurer la sécurité et la confidentialité des données collectées.
+        Nous prenons des mesures techniques et organisationnelles appropriées pour protéger vos informations personnelles contre tout accès non autorisé, toute divulgation, toute altération ou toute destruction.
+        
+        <h2> Modifications de la politique de confidentialité : </h2>
+        Nous nous réservons le droit de modifier cette politique de confidentialité à tout moment. Toute modification sera publiée sur cette page avec la date de révision mise à jour. Nous vous recommandons de consulter cette page régulièrement pour rester informé des modifications apportées à notre politique de confidentialité.
+        <br><br>En utilisant notre site web, vous consentez à notre politique de confidentialité et à nos pratiques en matière de traitement des données personnelles. Si vous avez des questions ou des préoccupations concernant notre politique de confidentialité, veuillez nous contacter à booking@we-escape.de.        
 
-        <h2> Partage des données </h2>
-        Les données personnelles des utilisateurs ne seront pas transmises à des tiers sans leur consentement préalable, sauf dans les cas prévus par la loi.
-
-        <h2> Droits des utilisateurs </h2>
-        Les utilisateurs disposent d'un droit d'accès, de rectification et de suppression de leurs données personnelles. Ils peuvent exercer ces droits en contactant le Prestataire aux coordonnées fournies dans les mentions légales.
-
-        <br><br>
-    </div>
+      </div>
 </div>
 
