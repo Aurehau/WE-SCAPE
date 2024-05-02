fa96dab898077a7a37566365c0cf0bc60aff0cc6
chaymabann <chayma.bann@outlook.fr>
2024-05-02 08:37:55 +0200

    update calendrier

--- script/calendrier.js
+++ script/calendrier.js
@@ -23,3 +23,21 @@ var calendar = new tui.Calendar('#calendar', {
     // Mettre à jour la valeur du champ de formulaire caché avec les dates sélectionnées
     selectedDatesInput.value = selectedDates.join(',');
 }); */
+
+
+/* calendar.on('clickDayname', function(event) {
+    var selectedDate = event.date;
+    var selectedDatesInput = document.getElementById('selectedDates');
+    var selectedDates = selectedDatesInput.value ? selectedDatesInput.value.split(',') : [];
+    var index = selectedDates.indexOf(selectedDate);
+
+    // Ajouter ou supprimer la date sélectionnée de la liste des dates sélectionnées
+    if (index === -1) {
+        selectedDates.push(selectedDate);
+    } else {
+        selectedDates.splice(index, 1);
+    }
+
+    // Mettre à jour la valeur du champ de formulaire caché avec les dates sélectionnées
+    selectedDatesInput.value = selectedDates.join(',');
+}); */
